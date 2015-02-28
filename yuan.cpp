f7246be7f87d284876a314492963b56668e97530
YuanZhou2015 <yuanzhou0924@gmail.com>
2015-02-28 00:22:00 -0600

    Header read and check
    header read and check using functions.

--- yuan.cpp
+++ yuan.cpp
@@ -2,8 +2,78 @@
 #include <iostream>
 #include <string>
 #include <ctype.h>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+
+void print_output(ofstream &outputfile, ofstream &logfile,string str,int &F){    
+    if (F==1){
+        outputfile.open("yuan.out");
+        cout << str;
+        outputfile << str;
+        outputfile.close();
+    }
+    if (F==2){
+        logfile.open("yuan.log");
+        cout << str;
+        logfile << str;
+        logfile.close();
+    }        
+}
+
+void open_log( string logfilename, ofstream &logfile){
+    //stringstream ss;
+    string information;
+    ofstream outputfile;
+    logfile.open(logfilename.c_str());
+    if (!logfile.is_open() ){
+        cout<< "Cannot open log file: "
+            << logfilename
+            << endl;
+        int flag = 1;
+        return;
+    }
+}
+
+void open_input(string inputfilename,ifstream &inputfile,int &flag){
+
+    ofstream outputfile, logfile;
+    stringstream ss;
+    string information;
+
+    inputfile.open(inputfilename.c_str());
+    if (!inputfile.is_open()){
+        ss << "Cannot open input file: "
+           << inputfilename
+           << endl;
+        information = ss.str();
+        int F = 1;
+        print_output(outputfile,logfile,information,F);
+        flag = 1;
+        return;
+    } 
+    return;
+}
+
+void open_output( string outputfilename, ofstream &outputfile){
+    ofstream logfile;
+    stringstream ss;
+    string information;
+
+    outputfile.open(outputfilename.c_str());
+    logfile.open("yuan.log");
+    if (!outputfile.is_open() ){
+        ss << "Cannot open output file: "
+             << outputfilename
+             << endl;
+        information = ss.str();
+        int F=2;
+        print_output(outputfile,logfile,information,F);
+        int flag = 1;
+        return;
+    }
+}
 
 
 void date_check(string date){
@@ -24,13 +94,13 @@ void date_check(string date){
     return;
 }
 
-void print_out_month(string date){
+string print_out_month(string date){
     if ((date[0]==48) && (date[1]==49))
-        cout << "January ";
+        return "January ";
     if ((date[0]==48) && (date[1]==50))
-        cout << "February ";
+        return "February ";
     if ((date[0]==48) && (date[1]==51))
-        cout << "March ";
+        return "March ";
     if ((date[0]==48) && (date[1]==52))
         cout << "April ";
     if ((date[0]==48) && (date[1]==53))
@@ -71,15 +141,109 @@ void time_check(string time){
     return;
 }
 
+void timezone_check(string timezone){
+    if (timezone.size()!=3){
+        cout <<"Invalid format of timezone.\n";
+        exit (0);
+    }
+    if (!isalpha(timezone[0])||!isalpha(timezone[1])||!isalpha(timezone[2])){
+        cout <<"Invalid format of timezone. \n";
+        exit(0);
+    }
+    return;
+}
+
+enum magnitude{ 
+    Ml, 
+    Ms, 
+    Mb, 
+    Mw 
+};
+// useful function to convert a string to uppercase 
+string uppercase (string &s){
+    string result = s;
+    for (int i=0; i<(int)result.size(); i++)
+        result[i] = toupper(result[i]);
+    return result;
+}
+
+bool is_valid_magnitude (string s) {
+    string ss = uppercase(s);
+    return((ss=="ML")||(ss=="MS")||(ss=="MB")||(ss=="MW"));
+}
 
 int main(){
-    string date;
-    cin >> date;
+    ifstream inputfile;
+    ofstream logfile, outputfile;
+    string inputfilename, logfilename, outputfilename;
+    string EventID, date, time, timezone,inform;
+  
+    int flag=0;
+
+    open_log("yuan.log",logfile);
+    logfile.close();
+    cout << "> Enter input file name: ";
+    cin >> inputfilename;
+    open_input(inputfilename,inputfile, flag);
+    if (flag ==1)
+    return 0;
+    inputfile >> EventID;
+    cout << EventID;
+    inputfile >> date;
     date_check(date);
+    cout << date[3] << date[4]<<' ';
     print_out_month(date);
-   /* string time;
-    cin >> time;
+    cout << date[6] << date[7] << date[8] << date[9] <<' ';   
+    inputfile >> time;
     time_check(time);
-    cout << time;*/
+    inputfile >> timezone;
+    timezone_check(timezone);
+    cout << timezone;
+    while (getline(inputfile,inform)){
+        break;
+    }
+    for ( int i=0; getline(inputfile,inform); i++ ){   
+        if (i==0)
+       // cout << inform << endl;
+        break;
+    }
+    double longitude, latitude,depth;
+    inputfile >> longitude >> latitude >> depth;
+    cout << longitude;
+    cout << latitude;
+    cout << depth;
+
+    string magnitude;
+    inputfile >> magnitude;
+    if(!is_valid_magnitude (magnitude)){
+        string information = "invalid manitude";
+        int F=2;
+        print_output(outputfile, logfile,information, F);
+        flag = 1;
+        return 0;
+    }
+    else
+        cout << magnitude;
+
+    string magnitudevalue;
+    inputfile >> magnitudevalue;
+    cout << magnitudevalue<<"\n";
+
+    //if (flag == 0)
+        open_output("yuan.out",outputfile);
+    outputfile.close();
+    stringstream ss;
+    ss << "# " << date[3] << date[4]<<' ';
+    ss << print_out_month(date);
+    ss << date[6] << date[7] << date[8] << date[9] <<' ';
+    ss << time << ' ' << timezone << ' ';
+    ss << magnitude << ' ' << magnitudevalue;
+    ss << inform << ' ';
+    ss << "[" << EventID <<"] ";
+    ss << "(" << longitude << ", " << latitude << ", " << depth << ")";
+    int F=1;
+    print_output(outputfile, logfile,ss.str(), F);
+
+
     return 0;
 }
