16927f196dfa83555f5017932a0f2b9fd3a3b029
YuanZhou2015 <yuanzhou0924@gmail.com>
2015-03-02 16:07:43 -0600

    Small file works, but has problem for larger one.

--- yuan.cpp
+++ yuan.cpp
@@ -1,4 +1,11 @@
-﻿#include <stdio.h>
+﻿/*Homework 5
+*
+
+* https://github.com/YuanZhou2015/Homework5
+* by Yuan Zhou
+* February 25 2015
+*/
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <ctype.h>
@@ -201,6 +208,307 @@ bool is_valid_magnitude (string s) {
     return((ss=="ML")||(ss=="MS")||(ss=="MB")||(ss=="MW"));
 }
 
+string itos(int i) {
+    stringstream s;
+    s << i;
+    return s.str();
+}
+
+void networkcode_check(string networkcode, stringstream &slog, stringstream &ss, int &i, int &flag){
+    string sout;
+    ofstream outputfile, logfile;
+    int F;
+    flag = 0;
+    if (networkcode.size()!=2){
+        sout = "Entry # ";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = itos(i);
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = " ignored. Invalid network. \n";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        flag=1;
+    }
+
+    if (networkcode!= "CE" && networkcode!= "CI" && networkcode!="FA" 
+        && networkcode!="NP" && networkcode!="WR"){
+        sout = "Entry # ";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout =itos (i);
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = " ignored. Invalid network. \n";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        flag=1;
+    }
+
+   /* if (flag==0){
+        F=1;
+        sout = '\0';
+        ss << networkcode<<'.';
+        print_output(outputfile, logfile,ss.str(),sout,F);
+    }*/
+    return;
+}
+
+void stationcode_check(string stationcode,stringstream &slog,stringstream &ss, int &i, int &flag){
+    string sout;
+    ofstream outputfile, logfile;
+    int F;
+    flag=0;
+    if (stationcode.size()!=3 && stationcode.size()!=5){
+        sout = "Entry # ";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = itos(i);
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = " ignored. Invalid station code. \n";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        flag=1;
+    }
+
+    if (stationcode.size()==3){
+        if (!isupper(stationcode[0])||!isupper(stationcode[1])||!isupper(stationcode[2])){
+            sout = "Entry # ";
+            slog << sout;
+            F=2;
+            print_output(outputfile, logfile, slog.str(), sout, F);
+            sout =itos (i);
+            slog << sout;
+            F=2;
+            print_output(outputfile, logfile, slog.str(), sout, F);
+            sout = " ignored. Invalid station code. \n";
+            slog << sout;
+            F=2;
+            print_output(outputfile, logfile, slog.str(), sout, F);
+            flag=1;
+        }
+    }
+
+    if (stationcode.size()==5){
+        if (!isdigit(stationcode[0])||!isdigit(stationcode[1])||!isdigit(stationcode[2])
+            ||!isdigit(stationcode[3])||!isdigit(stationcode[4])){
+            sout = "Entry # ";
+            slog << sout;
+            F=2;
+            print_output(outputfile, logfile, slog.str(), sout, F);
+            sout =itos (i);
+            slog << sout;
+            F=2;
+            print_output(outputfile, logfile, slog.str(), sout, F);
+            sout = " ignored. Invalid network. \n";
+            slog << sout;
+            F=2;
+            print_output(outputfile, logfile, slog.str(), sout, F);
+            flag=1;
+        }
+    }
+
+   /* if (flag==0){
+        F=1;
+        sout = '\0';
+        ss << stationcode<<'.';
+        print_output(outputfile, logfile,ss.str(),sout,F);
+    }*/
+    return;
+}
+
+bool is_valid_typeofband (string s) {
+    string ss = uppercase(s);
+    return((ss=="LONG-PERIOD")||(ss=="SHORT-PERIOD")||(ss=="VROADBAND"));
+}
+void typeofband_check(string &typeofband,stringstream &slog,stringstream &ss, int &i, int flag){
+    string sout;
+    ofstream outputfile, logfile;
+    int F;
+    flag=0;
+    if (!is_valid_typeofband(typeofband)){
+        sout = "Entry # ";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout =itos (i);
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = " ignored. Invalid band type. \n";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        flag=1;    
+    }
+    if (flag==0){
+        if (uppercase(typeofband)=="LONG-PERIOD"){
+            /*F=1;
+            sout = '\0';
+            ss << "L";
+            print_output(outputfile, logfile,ss.str(),sout,F);   */
+            typeofband = "L";
+        }
+        if (uppercase(typeofband)=="SHORT-PERIOD"){
+            /*F=1;
+            sout = '\0';
+            ss << "B";
+            print_output(outputfile, logfile,ss.str(),sout,F);   */
+            typeofband = "B";
+        }
+        if (uppercase(typeofband)=="BROADBAND"){
+           /* F=1;
+            sout = '\0';
+            ss << "H";
+            print_output(outputfile, logfile,ss.str(),sout,F);   */
+            typeofband = "H";
+        }
+    }
+
+}
+
+bool is_valid_typeofinstru (string s) {
+    string ss = uppercase(s);
+    return((ss=="HIGH-GAIN")||(ss=="LOW-GAIN")||(ss=="ACCELEROMETER"));
+}
+void typeofinstru_check(string &typeofinstru,stringstream &slog, stringstream &ss, int&i, int &flag){
+    string sout;
+    ofstream outputfile, logfile;
+    int F;
+    flag=0;
+    if (!is_valid_typeofinstru(typeofinstru)){
+        sout = "Entry # ";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout =itos (i);
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = " ignored. Invalid type of instrument. \n";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        flag=1;    
+    }
+    if (flag==0){
+        if (uppercase(typeofinstru)=="HIGH-GAIN"){
+           /* F=1;
+            sout = '\0';
+            ss << "H";
+            print_output(outputfile, logfile,ss.str(),sout,F);*/  
+            typeofinstru = "H";
+        }
+        if (uppercase(typeofinstru)=="LOW-GAIN"){
+           /* F=1;
+            sout = '\0';
+            ss << "L";
+            print_output(outputfile, logfile,ss.str(),sout,F);   */
+            typeofinstru = "L";
+        }
+        if (uppercase(typeofinstru)=="ACCELEROMETER"){
+            /*F=1;
+            sout = '\0';
+            ss << "N";
+            print_output(outputfile, logfile,ss.str(),sout,F);   */
+            typeofinstru = "N";
+        }
+    }
+}
+
+void orientation_check(string orientation,stringstream &slog,stringstream &ss, int &i, int &flag){
+    string sout;
+    ofstream outputfile, logfile;
+    int F,n,j;
+    flag=0;
+    if (orientation.size()>3||orientation.size()<1){
+        sout = "Entry # ";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = itos(i);
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        sout = " ignored. Invalid orientation. \n";
+        slog << sout;
+        F=2;
+        print_output(outputfile, logfile, slog.str(), sout, F);
+        flag=1;
+    }
+    else{
+        j=0;
+        n=orientation.size();
+        orientation = uppercase (orientation);
+        while(j<n){
+            if (orientation[j]!= '1' && orientation[j]!='2' && orientation[j]!= '3'){
+                while (j<n){
+                    if (orientation[j]!='N' && orientation[j]!='E' && orientation[j]!= 'Z'){
+                        sout = "Entry # ";
+                        slog << sout;
+                        F=2;
+                        print_output(outputfile, logfile, slog.str(), sout, F);
+                        sout = itos(i);
+                        slog << sout;
+                        F=2;
+                        print_output(outputfile, logfile, slog.str(), sout, F);
+                        sout = " ignored. Invalid orientation. \n";
+                        slog << sout;
+                        F=2;
+                        print_output(outputfile, logfile, slog.str(), sout, F);
+                        flag=1;
+                        j++;
+                    }
+                    else
+                        j++;
+                }
+            }
+            else{
+                j++;
+                while(j<n){
+                    if (orientation[j]!= '1' && orientation[j]!='2' && orientation[j]!= '3'){
+                        sout = "Entry # ";
+                        slog << sout;
+                        F=2;
+                        print_output(outputfile, logfile, slog.str(), sout, F);
+                        sout = itos(i);
+                        slog << sout;
+                        F=2;
+                        print_output(outputfile, logfile, slog.str(), sout, F);
+                        sout = " ignored. Invalid orientation. \n";
+                        slog << sout;
+                        F=2;
+                        print_output(outputfile, logfile, slog.str(), sout, F);
+                        flag=1;
+                        j++;
+                    }
+                    else
+                        j++;
+                }
+            }
+        }
+    }
+}
+
+struct Signal {
+    string NT;
+    string STN;
+    string B;
+    string I;
+    string O;
+};
+
 int main(){
     ifstream inputfile;
     ofstream logfile, outputfile;
@@ -251,15 +559,13 @@ int main(){
     string magnitude;
     inputfile >> magnitude;
     if(!is_valid_magnitude (magnitude)){
-        sout = "invalid magnitude.\n";
+        sout = "Invalid magnitude.\n";
         slog << sout;
         F=2;
         print_output(outputfile, logfile, slog.str(), sout, F);
         flag = 1;
         return 0;
     }
- /*   else
-        cout << magnitude;*/
 
     float magnitudevalue;
     inputfile >> magnitudevalue;
@@ -274,7 +580,7 @@ int main(){
 
     // If the header read successfully, then open the output file.
 
-    //if (flag == 0)
+    if (flag == 0)
         open_output("yuan.out",outputfile);
     outputfile.close();
     sout = "Header read correctly!\n";
@@ -289,10 +595,80 @@ int main(){
     ss << magnitude << ' ' << magnitudevalue << ' ';
     ss << name << "\n";
     ss << "[" << EventID <<"] ";
-    ss << "(" << longitude << ", " << latitude << ", " << depth << ")";
+    ss << "(" << longitude << ", " << latitude << ", " << depth << ")\n";
     F=1;
     print_output(outputfile, logfile,ss.str(),sout, F);
 
+    // Reading the table of earthquake information. The number of valid information will not more than 300.
+    const int MAXSIZE = 300;
+    Signal Signaldata[MAXSIZE];
+    int size = 0, i=1,m=0, flag1=0,flag2=0,flag3=0,flag4=0,flag5=0;
+    string networkcode, stationcode, typeofband, typeofinstru, orientation;
+    while (!inputfile.eof() && size< MAXSIZE ){              
+        inputfile >> networkcode;
+        networkcode_check(networkcode,slog,ss,i,flag1);
+        inputfile >> stationcode;
+        stationcode_check(stationcode,slog,ss,i,flag2);
+        inputfile >> typeofband;
+        typeofband_check(typeofband,slog,ss,i,flag3);
+        inputfile >> typeofinstru;
+        typeofinstru_check(typeofinstru,slog,ss,i,flag4);
+        inputfile >> orientation;
+        int n = orientation.size(), j=0;
+        orientation_check(orientation,slog,ss,i,flag5);
+
+        if(flag1==0 && flag2==0 && flag3==0 && flag4==0 && flag5==0){
+            while (j<n){
+                Signaldata[size].NT = networkcode;
+                Signaldata[size].STN = stationcode;
+                Signaldata[size].B = typeofband;
+                Signaldata[size].I = typeofinstru;
+                Signaldata[size].O = orientation[j];
+                //ss<< Signaldata[size].NT << "." << Signaldata[size].STN << "." << Signaldata[size].B << Signaldata[size].I << Signaldata[size].O <<"\n"; 
+                size++;
+                j++;
+            }
+        }
+        if(flag1!=0 || flag2!=0 || flag3!=0 || flag4!=0 || flag5!=0)
+            m++;    
+        i++;
+    }
+    int k=i;
+
+    F=2;
+    sout ="\nTotal invalid entries igored: ";
+    slog << sout;
+    print_output(outputfile, logfile,slog.str(),sout,F); 
+    sout =itos(m);
+    slog << sout; 
+    print_output(outputfile, logfile,slog.str(),sout,F); 
+    sout ="\nTotal valid entries read: ";
+    slog << sout;
+    print_output(outputfile, logfile,slog.str(),sout,F);
+    sout =itos(k-m-1);
+    slog << sout; 
+    print_output(outputfile, logfile,slog.str(),sout,F);
+    sout ="\nSignal names produced: ";
+    slog << sout;
+    print_output(outputfile, logfile,slog.str(),sout,F);
+    sout = itos(size);
+    slog << sout <<"\n";
+    print_output(outputfile, logfile,slog.str(),sout,F);
+    F=1;
+    sout = '\0';
+    ss << itos(size) <<"\n";
+    print_output(outputfile, logfile,ss.str(),sout,F); 
+    
+    for (int j=0; j< size; j++){
+        F=1;
+       // sout = '\0';
+        ss << Signaldata[j].NT << "." << Signaldata[j].STN << "." << Signaldata[j].B << Signaldata[j].I << Signaldata[j].O <<"\n"; 
+        print_output(outputfile, logfile,ss.str(),sout,F); 
+    }
+    F=2;
+    sout = "\nFinished!\n";
+    slog << sout;
+    print_output(outputfile, logfile,slog.str(),sout,F);
 
     return 0;
 }
